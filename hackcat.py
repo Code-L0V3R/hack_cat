eb91c8debb3ef93e83bdd351f439cd579175161c
L0V3R <Code-L0V3R@users.noreply.github.com>
2016-04-27 23:20:03 +0630

    Create hackcat.py

---
+++ hackcat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# L0V3R IN MYANMAR
+# HACKCAT
+import random,string
+from time import sleep
+a = string.letters+string.digits
+def c(t):
+	ran = lambda x: random.choice(x)
+	print ran(t) , ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t), ran(t)
+	sleep(0.05)
+try:
+	while True:
+		c(a)
+except KeyboardInterrupt:
+	exit(1)
